5f670d043f04cbc9b9aae184e0885b6d614fad88
Anna W <anski.anna@gmail.com>
2019-05-29 14:03:50 +0100

    got the components up for todos. Rewriting the components so I understand it better

--- src/App.js
+++ src/App.js
@@ -1,16 +1,77 @@
 import React from 'react';
 
+const todoList = [
+  {
+    task: 'Organize Garage',
+    id: 1528817077286,
+    completed: false
+  },
+  {
+    task: 'Bake Cookies',
+    id: 1528817084358,
+    completed: false
+  }
+];
+
+function Todos ({ todoList }) {
+  return (
+    <div>
+      {todoList.map(todo => {
+        return (
+          <div>
+            {todo.task}
+          </div>
+        )
+      })}
+    </div>
+  )
+}
+
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
-  render() {
+  constructor(props){
+    super(props)
+    this.state = {todoList}
+    
+  }
+  
+  render(){
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <Todos todoList={this.state.todoList} />
       </div>
-    );
+    )
   }
 }
 
 export default App;
+
+
+
+// function Todos ({ todoList }){
+//   return (
+//     <div>
+//     {todoList.map(todo => {
+//       return (
+//         <div>
+//           {todo.task}
+//         </div>
+//       )
+//     })}</div>
+//   )
+// }
+
+// constructor( props ){
+//   super(props)
+//   this.state = {todoList}
+// }
+
+// render() {
+//   return (
+//     <div>
+//       <Todos todoList={this.state.todoList}/>
+//     </div>
+//   );
+// }
