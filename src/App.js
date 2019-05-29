c174ea6e0c84b47e72578b39e87193e003f52ee0
Anna W <anski.anna@gmail.com>
2019-05-29 18:42:09 +0100

    tried to add some moroe functionality to mark as complete, no success yet. Also added a placeholder for the input

--- src/App.js
+++ src/App.js
@@ -13,6 +13,12 @@ const todoList = [
   }
 ];
 
+// removeTask = (id) => {
+//   this.setState({
+//     todoList: this.state.todoList.filter(task => task.id !== id)
+//   });
+// }
+
 function Todos ({todoList}){
   return(
     <div>
@@ -20,6 +26,7 @@ function Todos ({todoList}){
         return (
           <div key={todo.id}>
             {todo.task}
+          {/* <button onClick={() => this.removeTask(todo.id)}>delete</button> */}
           </div>  
         )
       })}
@@ -38,19 +45,19 @@ class App extends React.Component {
       newTodos: '',
     }
   }
+  
   changeHandler = (event) => {
       this.setState({
         newTodos: event.target.value,
       });
   }
 
-  addTodos =() => {
+  addTodos = () => {
     const newToDo ={
       id: Date.now(),
       task: this.state.newTodos,
       completed: false,
     };
-
 
     this.setState({
       todoList: this.state.todoList.concat(newToDo),
@@ -58,33 +65,62 @@ class App extends React.Component {
      })
   }
 
+  crossOut = data => {
+    const { todoList } = this.state;
+    const newData = data;
+    newData.completed = !newData.completed;
+
+    this.setState({
+      todokList: todoList.map(task => {
+        if (task.id === newData.id) {
+          return newData;
+        }
+        return task;
+      }),
+    });
+  };
+ 
+      
+
   render(){
     return (
       <div>
         <h3>Todo List</h3>
-        <Todos todoList={this.state.todoList} />
+        <Todos 
+        todoList={this.state.todoList} />
         <TodoAdder 
         newTodos={this.state.newTodos}
         changeHandler={this.changeHandler}
         addTodos={this.addTodos}
+        crossOut={this.crossOut}
         />
        </div>
     )
   }
 }
 
-function TodoAdder({ newTodos, changeHandler, addTodos}) {
+function TodoAdder({ newTodos, changeHandler, addTodos, crossOut }) {
   return (
     <div>
       <input
         value={newTodos}
         onChange={changeHandler}
         type="text"
+        placeholder="...todo"
       />
-      <button onClick={addTodos}>New Task!</button>
+      <button onClick={addTodos}>Add Task</button>
+      <button onClick={crossOut}>Clear completed</button>
     </div>
   )
 }
+
+// function ToggleTask ({ }){
+//   return (
+//     <div>
+
+//     </div>
+//   )
+// }
 
 export default App;
 
